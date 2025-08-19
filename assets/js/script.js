c20717f2d106cf0ff03913ddd116aad02838c176
Your Name <nickjacencube@gmail.com>
2025-08-19 15:59:48 +0100

    adding api key

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var apiKey = "4557393f83ef3c50705313a9e5378d4e";
+// jQuery selectors for various elements
+var searchForm = $("#search-form");
+var Input = $("input");
+var historyList = $("#history");
+var todaySection = $("#today");
+var forecastSection = $("#forecast");
+// Flag for temperature unit (Celsius/Fahrenheit)
+var isCelsius = true;
+
+
+
